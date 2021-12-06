0c5d8c759b788360738452990178ce0b88c99747
Manuel Real <89550044+IOManuelReal@users.noreply.github.com>
2021-12-06 15:42:42 -0800

    Create ejercicio7.s

---
+++ capitulo-dos/ejercicio7.s
@@ -0,0 +1,9 @@
+
+@@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. en Sistemas Computacionales
+@@ Autor: Manuel Angel Real Castro  @nickname: IOManuelReal
+@@ Fecha de revision: 06/12/2021
+
+
+@@ Nombre: ejercicio7.s
