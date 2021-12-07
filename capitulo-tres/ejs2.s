45fe4ef253c305e2b39cb815186f56f4ebaf34bc
Manuel Real <89550044+IOManuelReal@users.noreply.github.com>
2021-12-07 13:58:00 -0800

    Update ejs2.s

--- capitulo-tres/ejs2.s
+++ capitulo-tres/ejs2.s
@@ -1,1 +1,15 @@
-@@
+
+@@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. en Sistemas Computacionales
+@@ Autor: Manuel Angel Real Castro  @nickname: IOManuelReal
+@@ Fecha de revision: 07/12/2021
+
+/*
+* Escribe una función en ensamblador que calcule la media aritmética (truncada
+* porque trabajamos con enteros) de dos números. Escribe también la función main
+* con cinco llamadas a media con distintos parámetros.
+*/
+
+@@ Nombre: ejs2.s
+
