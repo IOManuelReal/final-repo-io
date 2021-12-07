bb779224488b8f3481a2ad790a05dcb7b04962bd
Manuel Real <89550044+IOManuelReal@users.noreply.github.com>
2021-12-07 13:56:52 -0800

    Update ejs1.s

--- capitulo-tres/ejs1.s
+++ capitulo-tres/ejs1.s
@@ -1,1 +1,15 @@
+
 @@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. en Sistemas Computacionales
+@@ Autor: Manuel Angel Real Castro  @nickname: IOManuelReal
+@@ Fecha de revision: 07/12/2021
+
+/*
+* Dado un vector de enteros y su longitud, escribe una función en ensamblador
+* que recorra todos los elementos del vector y nos devuelva el valor mínimo. Para
+* comprobar su funcionamiento, haz .global la función y tras ensamblarla, enlázala
+* con este programa en C.
+*/
+
+@@ Nombre: ejs1.s
