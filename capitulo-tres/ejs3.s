7edf7503fdcd20ed3886ce238aa75b69af5f4172
Manuel Real <89550044+IOManuelReal@users.noreply.github.com>
2021-12-07 13:59:43 -0800

    Update ejs3.s

--- capitulo-tres/ejs3.s
+++ capitulo-tres/ejs3.s
@@ -1,1 +1,17 @@
-@@ 
+
+@@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. en Sistemas Computacionales
+@@ Autor: Manuel Angel Real Castro  @nickname: IOManuelReal
+@@ Fecha de revision: 07/12/2021
+
+/*
+* Escoge un algoritmo de ordenación de entre los 4 siguientes e impleméntalo en
+* ensamblador:
+*   Burbuja.
+*   Selección.
+*   Inserción.
+*   Quicksort.
+*/
+
+@@ Nombre: ejs3.s 
