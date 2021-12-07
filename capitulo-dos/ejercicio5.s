cb4985e5dc5192409aa1ec259a8887d298515aca
Manuel Real <89550044+IOManuelReal@users.noreply.github.com>
2021-12-06 20:53:42 -0800

    Delete ejercicio5.s

--- capitulo-dos/ejercicio5.s
+++
@@ -1,9 +0,0 @@
-
-@@ Instituto Tecnologico de Tijuana
-@@ Depto de Sistemas y Computacion
-@@ Ing. en Sistemas Computacionales
-@@ Autor: Manuel Angel Real Castro  @nickname: IOManuelReal
-@@ Fecha de revision: 06/12/2021
-
-
-@@ Nombre: ejercicio5.s
